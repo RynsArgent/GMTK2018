069821f9e848578b9178cbdc92c7932936c83232
Evan Shen <evanshen2010@gmail.com>
2018-09-01 02:51:24 -0500

    Got path following working
    Also added animation triggers for the slime

--- Capture Tower/Assets/Scripts/Path.cs
+++ Capture Tower/Assets/Scripts/Path.cs
@@ -4,34 +4,14 @@ using UnityEngine;
 
 public class Path : MonoBehaviour {
 
-	public Transform[] wayPointsList;
-	public int currentWayPoint = 0;
-	public float speed = 3f;
-
-	private Transform targetWaypoint;
-	private Transform unitObjTransform;
+	public Transform[] waypointList;
 
 	// Use this for initialization
 	void Awake () {
-		unitObjTransform = gameObject.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (currentWayPoint < wayPointsList.Length) {
-			if (targetWaypoint == null)
-				targetWaypoint = wayPointsList[currentWayPoint];
-			Move();
-		}
 		
-	}
-
-	private void Move() {
-		unitObjTransform.position = Vector3.MoveTowards(unitObjTransform.position, targetWaypoint.position, speed * Time.deltaTime);
-
-		if (unitObjTransform.position == targetWaypoint.position) {
-			currentWayPoint++;
-			targetWaypoint = wayPointsList[currentWayPoint];
-		}
 	}
 }
--- Capture Tower/Assets/Scripts/Unit.cs
+++ Capture Tower/Assets/Scripts/Unit.cs
@@ -5,20 +5,55 @@ using UnityEngine;
 public class Unit : MonoBehaviour {
 
 	public bool isFriendly = false;
-	public Path path;
 	public float speed = 3f;
+
+	private SpriteRenderer spriteRenderer;
+	private Animator animator;
+	private Path path;
+	private Transform[] waypoints;
+	private int currentWp = 0;
+	private Transform targetWp;
 
 	// Use this for initialization
 	private void Awake() {
-		path = GetComponent<Path>();
 	}
 
 	void Start () {
-		
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		animator = GetComponent<Animator>();
+		path = GameObject.Find("Path").GetComponent<Path>();
+		waypoints = path.waypointList;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		if (currentWp < waypoints.Length) {
+			if (targetWp == null)
+				targetWp = waypoints[currentWp];
+
+			animator.SetBool("Move", true);
+			if (transform.position.x < targetWp.position.x) {
+				spriteRenderer.flipX = true;
+			} else {
+				spriteRenderer.flipX = false;
+			}
+
+			MoveAlongPath();
+		}
+	}
+
+	private void MoveAlongPath()
+	{
+		transform.root.position = Vector3.MoveTowards(transform.root.position, targetWp.position, speed * Time.deltaTime);
+
+		if (transform.root.position == targetWp.position) {
+			currentWp++;
+			if (currentWp == waypoints.Length) {
+				animator.SetBool("Move", false);
+				return;
+			}
+			targetWp = waypoints[currentWp];
+		}
 	}
 }
