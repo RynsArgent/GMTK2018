649a563e607ab4efafe6e7099e03d9bcbc9bc836
blacknight <blacknight7890@gmail.com>
2018-09-01 15:21:23 -0400

    laser push

---
+++ Capture Tower/Assets/scripts/mousefollow.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class mousefollow : MonoBehaviour {
+        // Update is called once per frame
+        void Update () {
+        Vector2 temp = Input.mousePosition;
+        transform.position = Camera.main.ScreenToWorldPoint(temp);
+    } 
+}
--- Capture Tower/Assets/scripts/scr_laserpoint.cs
+++ Capture Tower/Assets/scripts/scr_laserpoint.cs
@@ -3,20 +3,19 @@
 public class scr_laserpoint : MonoBehaviour {
     private LineRenderer linerenderer;
     public Transform Tower;
+    public Vector3 offset;
 	// Use this for initialization
 	void Start () {
         linerenderer = GetComponent<LineRenderer>();
         linerenderer.enabled = false;
         linerenderer.useWorldSpace = true;
-        transform.position = Tower.position;
+        transform.position = Tower.position + offset;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
-        //laserpoint.position = hit.point;
-        linerenderer.SetPosition(0, transform.position);
-        linerenderer.SetPosition(1, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        linerenderer.SetPosition(0, (Vector2)transform.position);
+        linerenderer.SetPosition(1, (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition));
         if (Input.GetKey("mouse 0"))
         {
             linerenderer.enabled = true;
