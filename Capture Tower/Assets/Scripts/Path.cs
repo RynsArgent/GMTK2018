62dcdeb336bfb67fe7eb1053a2b53fa030293610
Evan Shen <evanshen2010@gmail.com>
2018-08-31 23:23:37 -0500

    New files for units and paths

---
+++ Capture Tower/Assets/Scripts/Path.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+
+	public Transform[] wayPointsList;
+	public int currentWayPoint = 0;
+
+	private Transform targetWaypoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentWayPoint < wayPointsList.Length) {
+			if (targetWaypoint == null)
+				targetWaypoint = wayPointsList[currentWayPoint];
+			move();
+		}
+		
+	}
+
+	private void move() {
+
+	}
+}
