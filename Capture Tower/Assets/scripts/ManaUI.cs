baf99bdd331759c04b56fbceaf27e841790be6fa
blacknight <blacknight7890@gmail.com>
2018-09-01 17:11:07 -0400

    UI attempt

---
+++ Capture Tower/Assets/scripts/ManaUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ManaUI : MonoBehaviour {
+    Text Mana;
+    // Update is called once per frame
+    void Update () {
+        Mana.text = "Mana: " + scr_laserpoint.Mana;
+	}
+}
--- Capture Tower/Assets/scripts/scr_laserpoint.cs
+++ Capture Tower/Assets/scripts/scr_laserpoint.cs
@@ -4,27 +4,41 @@ public class scr_laserpoint : MonoBehaviour {
     private LineRenderer linerenderer;
     public Transform Tower;
     public Vector3 offset;
-	// Use this for initialization
-	void Start () {
+    public static int Mana;
+    // Use this for initialization
+    void Start() {
         linerenderer = GetComponent<LineRenderer>();
         linerenderer.enabled = false;
         linerenderer.useWorldSpace = true;
         transform.position = Tower.position + offset;
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
         linerenderer.SetPosition(0, (Vector2)transform.position);
         linerenderer.SetPosition(1, (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        if (Input.GetKey("mouse 0"))
+
+    if (Input.GetKey("mouse 0"))
         {
+        if (Mana > 0)
+            {
             linerenderer.enabled = true;
+            Mana -= 2;
+            }
         }
-        else
+    else
         {
-            linerenderer.enabled = false;
+        linerenderer.enabled = false;
+       if (Mana < 200)
+        Mana += 1;
         }
 
+    if (Mana <= 0)
+        {
+        linerenderer.enabled = false;
+        }
 
+        
     }
 }
