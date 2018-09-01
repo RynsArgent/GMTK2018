f1a6fc4d617faf05db5ead6ce1ba7ed504815ba1
Calvin Phung <RynsArgent@hotmail.com>
2018-08-31 23:02:35 -0700

    CP Setup continuous waves

--- Capture Tower/Assets/Source/WaveManager.cs
+++ Capture Tower/Assets/Source/WaveManager.cs
@@ -7,14 +7,35 @@ public class WaveManager : MonoBehaviour
 {
     // Waves in order.
     private List<PlayableDirector> waves;
-
+    int currentWaveIndex;
+    
     public void Awake()
     {
         waves = new List<PlayableDirector>(GetComponentsInChildren<PlayableDirector>());
+        currentWaveIndex = 0;
     }
 
     public void Start()
     {
-        waves[0].Play();
+        StartWave();
+    }
+
+    public void StartWave()
+    {
+        if (currentWaveIndex >= waves.Count)
+        {
+            Debug.Log("FINISHED ALL WAVES");
+            return;
+        }
+
+        waves[currentWaveIndex].Play();
+        waves[currentWaveIndex].stopped += OnWaveFinished;
+    }
+
+    public void OnWaveFinished(PlayableDirector wave)
+    {
+        Debug.Log("Finished Wave " + currentWaveIndex.ToString());
+        currentWaveIndex++;
+        StartWave();
     }
 }
