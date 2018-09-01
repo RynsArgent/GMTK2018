3151c52031ee934744328fb9ec2492167f39e258
Evan Shen <evanshen2010@gmail.com>
2018-09-01 11:39:40 -0500

    Turned Unit into a superclass Ally and Enemy inherit
    And moved pathing alogic into Enemy

--- Capture Tower/Assets/Scripts/Unit.cs
+++ Capture Tower/Assets/Scripts/Unit.cs
@@ -2,58 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour {
+public abstract class Unit : MonoBehaviour {
 
-	public bool isFriendly = false;
 	public float speed = 3f;
 
-	private SpriteRenderer spriteRenderer;
-	private Animator animator;
-	private Path path;
-	private Transform[] waypoints;
-	private int currentWp = 0;
-	private Transform targetWp;
+	protected SpriteRenderer spriteRenderer;
+	protected Animator animator;
 
 	// Use this for initialization
-	private void Awake() {
-	}
-
-	void Start () {
+	void Awake() {
 		spriteRenderer = GetComponent<SpriteRenderer>();
 		animator = GetComponent<Animator>();
-		path = GameObject.Find("Path").GetComponent<Path>();
-		waypoints = path.waypointList;
 	}
-	
+
 	// Update is called once per frame
-	void Update ()
-	{
-		if (currentWp < waypoints.Length) {
-			if (targetWp == null)
-				targetWp = waypoints[currentWp];
-
-			animator.SetBool("Move", true);
-			if (transform.position.x < targetWp.position.x) {
-				spriteRenderer.flipX = true;
-			} else {
-				spriteRenderer.flipX = false;
-			}
-
-			MoveAlongPath();
-		}
-	}
-
-	private void MoveAlongPath()
-	{
-		transform.root.position = Vector3.MoveTowards(transform.root.position, targetWp.position, speed * Time.deltaTime);
-
-		if (transform.root.position == targetWp.position) {
-			currentWp++;
-			if (currentWp == waypoints.Length) {
-				animator.SetBool("Move", false);
-				return;
-			}
-			targetWp = waypoints[currentWp];
-		}
+	void Update() {
 	}
 }
