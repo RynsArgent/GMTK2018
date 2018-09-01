514dcaa6131db4b8058a59483b4fe9761770ba2b
Calvin Phung <RynsArgent@hotmail.com>
2018-08-31 22:20:09 -0700

    CP Setup simple wave timeline

---
+++ Capture Tower/Assets/Source/WaveManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class WaveManager : MonoBehaviour
+{
+    // Waves in order.
+    private List<PlayableDirector> waves;
+
+    public void Awake()
+    {
+        waves = new List<PlayableDirector>(GetComponentsInChildren<PlayableDirector>());
+    }
+
+    public void Start()
+    {
+        waves[0].Play();
+    }
+}
