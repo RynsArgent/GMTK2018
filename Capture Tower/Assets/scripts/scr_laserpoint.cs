be1691636189a9a3871e6d3fa2eacc1618c96d57
blacknight <blacknight7890@gmail.com>
2018-09-01 11:02:19 -0400

    tower laser done
    Finally functional, shoots to where you click and folows your mouse.

--- Capture Tower/Assets/scripts/scr_laserpoint.cs
+++ Capture Tower/Assets/scripts/scr_laserpoint.cs
@@ -2,20 +2,21 @@
 
 public class scr_laserpoint : MonoBehaviour {
     private LineRenderer linerenderer;
-    public Transform laserPoint;
+    public Transform Tower;
 	// Use this for initialization
 	void Start () {
         linerenderer = GetComponent<LineRenderer>();
         linerenderer.enabled = false;
         linerenderer.useWorldSpace = true;
+        transform.position = Tower.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Input.mousePosition);
-        laserPoint.position = hit.point;
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+        //laserpoint.position = hit.point;
         linerenderer.SetPosition(0, transform.position);
-        linerenderer.SetPosition(1, laserPoint.position);
+        linerenderer.SetPosition(1, Camera.main.ScreenToWorldPoint(Input.mousePosition));
         if (Input.GetKey("mouse 0"))
         {
             linerenderer.enabled = true;
